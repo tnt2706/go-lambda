9ad303096bc594a85198be9f10a845095d96ff84
tinhtn-3406 <tran.ngoc.tinh@sun-asterisk.com>
2024-09-15 20:39:34 +0700

    Implement auto start/stop instance use eventBridge and lambda

--- cmd/ec2/main.go
+++ cmd/ec2/main.go
@@ -1,1 +1,52 @@
-package ec2
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"log"
+)
+
+var ec2Client *ec2.Client
+
+type Event struct {
+	InstanceID string `json:"instance_id"`
+	Action     string `json:"action"`
+}
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("Unable to load SDK config: %v", err)
+	}
+	ec2Client = ec2.NewFromConfig(cfg)
+}
+
+func handler(ctx context.Context, event Event) {
+	if event.Action == "STOP" {
+		_, err := ec2Client.StopInstances(ctx, &ec2.StopInstancesInput{
+			InstanceIds: []string{event.InstanceID},
+			Force:       aws.Bool(false),
+		})
+
+		if err != nil {
+			log.Fatalf("Unable stop instances: %v", err)
+			return
+		}
+	} else {
+		_, err := ec2Client.StartInstances(ctx, &ec2.StartInstancesInput{
+			InstanceIds: []string{event.InstanceID},
+		})
+
+		if err != nil {
+			return
+		}
+	}
+
+}
+
+func main() {
+	lambda.Start(handler)
+}
